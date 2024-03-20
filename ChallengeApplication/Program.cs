705e7a950ebefeeff1c118a911d6cd3485302579
Bartosz Gorski <bartosz.gorski1998@gmail.com>
2024-03-20 18:27:01 +0100

    Zadanie domowe dzień 4.

--- ChallengeApplication/Program.cs
+++ ChallengeApplication/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Witaj świecie!");
+﻿string name = "Ewa";
+string sex = "Kobieta";
+int age = 19;
+
+if (age < 30 && sex == "Kobieta")
+{
+    Console.WriteLine("Kobieta poniżej 30 roku życia");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine($"{name}, lat {age}");
+}
+else if (age < 18 && sex == "Mężczyzna")
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
