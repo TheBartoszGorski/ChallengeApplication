dad1cca81305e7e2ad11ac98d5646f31d69b1aa8
Bartosz Gorski <bartosz.gorski1998@gmail.com>
2024-03-21 19:35:01 +0100

    Zadanie domowe dzień 6.

---
+++ ChallengeApplication/Employee.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeApplication
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public List<int> Grades = new List<int>();
+
+        public Employee(string name, string surname, int age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+
+        public void AddGrade(int grade)
+        {
+            if (grade < 0)
+            {
+                Grades.Add(0);
+            }
+            else if (grade > 10)
+            {
+                Grades.Add(10);
+            }
+            else
+            {
+                Grades.Add(grade);
+            }
+        }
+
+        public int GetGradeSum()
+        {
+            int sum = 0;
+
+            foreach (var grade in Grades)
+            {
+                sum += grade;
+            }
+
+            return sum;
+        }
+
+        public void DisplayEmployeeInformation()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Surname: {Surname}");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine($"Employee score: {this.GetGradeSum()}");
+        }
+    }
+}
--- ChallengeApplication/Program.cs
+++ ChallengeApplication/Program.cs
@@ -1,38 +1,77 @@
-﻿// Exercise 5
-// Count the number of occurences of each digit in a chosen number.
+﻿// Exercise 6
+using ChallengeApplication;
 
-long number = 772366991;
+Employee employee1 = new Employee("Arek", "Hlipała", 23);
+Employee employee2 = new Employee("Jarek", "Kaczkowski", 40);
+Employee employee3 = new Employee("Andrzej", "Twardowski", 31);
 
-string numberAsString = number.ToString();
-char[] numberAsArray = numberAsString.ToCharArray();
-char[] uniqueNumbers = numberAsArray.Distinct().ToArray();
+List<Employee> listOfEmployees = new List<Employee>();
 
-char firstASCIICharacterAfterDigits = ':';
+listOfEmployees.Add(employee1);
+listOfEmployees.Add(employee2);
+listOfEmployees.Add(employee3);
 
-for (char digit = '0'; digit < firstASCIICharacterAfterDigits; digit++)
+Random randomize = new Random();
+
+//Give each employee in the list 5 random grandes in range [1 10]
+for (int i = 0; i < 5; i++)
 {
-    bool isInNumber = false;
-
-    foreach (char c in uniqueNumbers)
+    foreach (Employee employee in listOfEmployees)
     {
-        if (c == digit)
+        employee.AddGrade(randomize.Next(1, 11));
+    }
+}
+
+DisplayEmployeesGradeSums(listOfEmployees);
+
+List<int> indexesOfBestEmployees = GetIndexesOfBestEmployees(listOfEmployees);
+
+if (indexesOfBestEmployees.Count == 1)
+{
+    Console.WriteLine("Employee of the month:");
+}
+else
+{
+    Console.WriteLine("Employees of the month:");
+}
+
+foreach (int index in indexesOfBestEmployees)
+{
+    listOfEmployees[index].DisplayEmployeeInformation();
+}
+
+List<int> GetIndexesOfBestEmployees(List<Employee> employeeList)
+{
+    int bestEmployeeScore = 0;
+    
+    foreach (Employee employee in listOfEmployees)
+    {
+        if (employee.GetGradeSum() > bestEmployeeScore)
         {
-            isInNumber = true;
+            bestEmployeeScore = employee.GetGradeSum();
         }
     }
 
     int counter = 0;
+    List<int> indexesOfBestEmployees = new();
 
-    if (isInNumber)
+    foreach (Employee employee in listOfEmployees)
     {
-        foreach (char c in numberAsArray)
+        if (employee.GetGradeSum() == bestEmployeeScore)
         {
-            if (c == digit)
-            {
-                counter++;
-            }
+            indexesOfBestEmployees.Add(counter);
         }
+
+        counter++;
     }
 
-    Console.WriteLine($"{digit} => {counter}");
+    return indexesOfBestEmployees;
 }
+void DisplayEmployeesGradeSums(List<Employee> employeeList)
+{
+    foreach (Employee employee in listOfEmployees)
+    {
+        Console.WriteLine($"Employee {employee.Name} {employee.Surname} grade sum :{employee.GetGradeSum()}");
+    }
+}
+
